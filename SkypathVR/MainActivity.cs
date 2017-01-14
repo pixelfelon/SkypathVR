a87ac9721a10ce14c5d7e9d1808a2e4bbeaa1971
James Rowley <james@jrowley.me>
2017-01-14 00:58:23 -0700

    Initial Xamarin Setup

---
+++ SkypathVR/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SkypathVR
+{
+    [Activity(Label = "SkypathVR", MainLauncher = true, Icon = "@drawable/temp_logo")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ SkypathVR/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: AssemblyTitle("SkypathVR")]
+[assembly: AssemblyDescription("Skypath VR Viewer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SkypathVR")]
+[assembly: AssemblyCopyright("Copyright ©2017 SkypathVR Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
