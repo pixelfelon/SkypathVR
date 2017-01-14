7e569388633793b3ec831b34aff97f6022775802
James Rowley <james@jrowley.me>
2017-01-14 15:30:10 -0700

    Basic MJPEG streaming over HTTP
    Source is some UofA webcam

--- SkypathVR/MainActivity.cs
+++ SkypathVR/MainActivity.cs
@@ -1,10 +1,15 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using MjpegProcessor;
+using System;
+using System.IO;
+using Android.Graphics.Drawables;
+using Android.Graphics;
 
 namespace SkypathVR
 {
-    [Activity(Label = "SkypathVR", MainLauncher = true, Icon = "@drawable/temp_logo")]
+    [Activity(Label = "Skypath VR", MainLauncher = true, Icon = "@drawable/temp_logo")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -12,6 +17,19 @@ namespace SkypathVR
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
+            ImageView output = FindViewById<ImageView>(Resource.Id.OutputImage);
+            
+            var decoder = new MjpegDecoder();
+            decoder.ParseStream(new System.Uri("http://webcam5.mmto.arizona.edu/mjpg/video.mjpg"));
+            Bitmap inter;
+            decoder.FrameReady += delegate (object sender, FrameReadyEventArgs FB)
+            {
+                //Stream inter = new MemoryStream(FB.FrameBuffer);
+                inter = BitmapFactory.DecodeByteArray(FB.FrameBuffer, 0, FB.FrameBuffer.Length);
+                //output.SetImageDrawable(Drawable.CreateFromStream(inter, "itemp"));
+                output.SetImageBitmap(inter);
+                
+            };
         }
     }
 }
